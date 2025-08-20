d6e2262f1619bb263005ac7aa19d5ec603a9ed97
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2025-08-20 07:25:23 +0530

    add registers

--- src/cpu/cpu.hpp
+++ src/cpu/cpu.hpp
@@ -20,9 +20,52 @@ private:
     u8 h;
     u8 l;
     u8 f;
+
+    u16 sp;
+    u16 pc;
+
+    u8 input;
+    u8 output;
 public:
     cpu(/* args */);
     ~cpu();
+
+    // get reg values
+    u8 get_a();
+    u8 get_b();
+    u8 get_c();
+    u8 get_d();
+    u8 get_e();
+    u8 get_h();
+    u8 get_l();
+    u8 get_f();
+
+    // set reg values
+    u8 set_a();
+    u8 set_b();
+    u8 set_c();
+    u8 set_d();
+    u8 set_e();
+    u8 set_h();
+    u8 set_l();
+    u8 set_f();
+
+
+    // 16 bit regs
+    u16 get_bc();
+    u16 get_de();
+    u16 get_hl();
+
+    u16 set_bc();
+    u16 set_de();
+    u16 set_hl();
+
+    // input output
+    u8 get_input();
+    u8 get_output();
+
+    u8 set_input();
+    u8 set_output();
 };
 
 cpu::cpu(/* args */)
